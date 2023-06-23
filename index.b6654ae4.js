4e62db7d89ce6a8ee672c37c42c22eaa2e0c9306
Andrii Kovalov <kowalowandrzej@gmail.com>
2023-06-23 18:58:07 +0200

    Updates

---
+++ index.b6654ae4.js
@@ -0,0 +1,2 @@
+let a;!async function(){let t={lat:52.157115,lng:21.030927},{Map:e}=await google.maps.importLibrary("maps"),{AdvancedMarkerElement:o}=await google.maps.importLibrary("marker");a=new e(document.getElementById("map"),{zoom:16,center:t,mapId:"DEMO_MAP_ID"}),new o({map:a,position:t,title:"Top Cut Barbershop"})}();
+//# sourceMappingURL=index.b6654ae4.js.map
