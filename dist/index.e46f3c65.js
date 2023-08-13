ad0984ff3082ef22465fd2b50772b0a1038b4ae4
Andrii Kovalov <kowalowandrzej@gmail.com>
2023-08-13 15:59:27 +0200

    updated

--- dist/index.e46f3c65.js
+++
@@ -1,2 +0,0 @@
-let e=0;!function l(){let t;let s=document.getElementsByClassName("mySlides"),a=document.getElementsByClassName("dot");for(t=0;t<s.length;t++)s[t].style.display="none";for(++e>s.length&&(e=1),t=0;t<a.length;t++)a[t].className=a[t].className.replace(" active","");s[e-1].style.display="block",a[e-1].className+=" active",setTimeout(l,5e3)}();
-//# sourceMappingURL=index.e46f3c65.js.map
